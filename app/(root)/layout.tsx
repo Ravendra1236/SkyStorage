88312a77859b6a3cb8a03833b06407ddd1dd8837
Ravendra1236 <ravendras189@gmail.com>
2025-02-01 22:49:57 +0530

    File-Details Uploaded

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import {Toaster} from "@/components/ui/toaster"
 import React from "react" ;
 
 const Layout =async ({children} : {children: React.ReactNode}) =>{
@@ -11,17 +12,14 @@ const Layout =async ({children} : {children: React.ReactNode}) =>{
       if (!currentUser) return redirect("/sign-in");
     return (
       <main className="flex h-screen">
-        {/* <Sidebar
-          fullName={currentUser.fullName}
-          avatar={currentUser.avatar}
-          email={currentUser.email}
-        /> */}
+        
         <Sidebar {...currentUser} />
         <section className="flex h-full flex-1 flex-col">
-          <MobileNavigation />
-          <Header />
+          <MobileNavigation {...currentUser}/>
+          <Header userId={currentUser.$id} accountId={currentUser.accountId}/>
           <div className="main-content">{children}</div>
         </section>
+        <Toaster/>
       </main>
     );
 }
